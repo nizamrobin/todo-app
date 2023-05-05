7cc3fe9d2260647c1803ece1a4967c2b83e4ee82
nizamrobin <nizamrobin@gmail.com>
2023-05-05 22:24:59 +0600

    Button type and handle function added

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,11 @@ import classes from "../styles/Button.module.css";
 
 export default function Button(props) {
   return (
-    <button className={`${classes.button} ${classes[props.btnType]}`}>
+    <button
+      type="button"
+      onClick={props.handleClick}
+      className={`${classes.button} ${classes[props.btnType]}`}
+    >
       <i class="fa-solid fa-plus"></i>
     </button>
   );
