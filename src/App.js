33caed8f3bd793f419c4d632c3f5813aea33a63e
nizamrobin <nizamrobin@gmail.com>
2023-05-05 22:45:46 +0600

    Button modified for fontAsesome icon prop

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Button from "./components/Button";
 export default function App() {
   return (
     <>
-      {/* <Button />; */}
+      <Button />;
       <BtnTodoAd />
     </>
   );
--- src/components/BtnTodoAdd.js
+++ src/components/BtnTodoAdd.js
@@ -1,5 +1,5 @@
 import Button from "./Button";
 
 export default function BtnTodoAd() {
-  return <Button btnType="btnTodoAdd" />;
+  return <Button btnType="btnTodoAdd" fontAwesome="fa-solid fa-plus" />;
 }
