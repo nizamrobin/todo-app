26c865fe97e5a3ada5cd6fc7048b44cd48c9b048
nizamrobin <nizamrobin@gmail.com>
2023-05-13 03:22:25 +0600

    Todo delete operation done

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,13 +3,14 @@ import classes from "../styles/Button.module.css";
 export default function Button(props) {
   const handleClick = (e) => {
     // e.preventDefault();
-    // console.log(e.currentTarget);
-    props.onClick(e.currentTarget);
+    // console.log(e.currentTarget.name);
+    props.onClick(e.currentTarget.name);
   };
   return (
     <button
       type="button"
       onClick={handleClick}
+      name={props.name}
       className={`${classes.button} ${classes[props.btnType]}`}
     >
       {props.children}
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,18 +1,28 @@
 import Button from "./Button";
 import classes from "../styles/ListItem.module.css";
+import { getDatabase, ref, remove } from "@firebase/database";
 
-export default function ListItem({ text }) {
+export default function ListItem({ text, id }) {
+  const db = getDatabase();
+  const handleClick = (e) => {
+    // Delete  an item from database
+    if (e === "delete") {
+      console.log(text);
+      remove(ref(db, id));
+    }
+  };
+
   return (
     <li className={classes.listItem}>
       <p className={classes.listItemText}>{text}</p>
       <div className={classes.listItemBtns}>
-        <Button onClick="handleClick" btnType="btnDone">
+        <Button onClick={handleClick} btnType="btnDone" name="done">
           <i class="fa-solid fa-check"></i>
         </Button>
-        <Button onClick="handleClick" btnType="btnEdit">
+        <Button onClick={handleClick} btnType="btnEdit" name="edit">
           <i class="fa-solid fa-pen"></i>
         </Button>
-        <Button onClick="handleClick" btnType="btnDelete">
+        <Button onClick={handleClick} btnType="btnDelete" name="delete">
           <i class="fa-solid fa-trash"></i>
         </Button>
       </div>
