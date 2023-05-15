8ddf10b3fed5f64d77a59fdcae3f7e28d1bbbe2c
nizamrobin <nizamrobin@gmail.com>
2023-05-15 20:27:33 +0600

    notification done

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,8 +2,6 @@ import classes from "../styles/Button.module.css";
 
 export default function Button(props) {
   const handleClick = (e) => {
-    // e.preventDefault();
-    // console.log(e.currentTarget.name);
     props.onClick(e.currentTarget.name);
   };
   return (
