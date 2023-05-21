6f29621d70fddd2ff65a2863f1b46f221b9edbe5
nizamrobin <nizamrobin@gmail.com>
2023-05-22 00:20:08 +0600

    Undo a deleted item done

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,40 +1,55 @@
 import Button from "./Button";
-// import Notification from "./Notification";
 import classes from "../styles/ListItem.module.css";
-import { getDatabase, ref, remove, update } from "@firebase/database";
+import { getDatabase, set, ref, remove, update } from "@firebase/database";
 import { useState } from "react";
 import TextArea from "./TextArea";
-
+import ListItemInfo from "./ListItemInfo";
 export default function ListItem({ text, id, notificationHandler }) {
-  // console.log(setNotification);
   const [edit, setEdit] = useState(false);
   const [todo, setTodo] = useState("");
-  // const [notification, setNotification] = useState(false);
+  const [undo, setUndo] = useState(false);
   const db = getDatabase();
 
+  // fetch the input of textarea input field of TextArea comp and handle it
   const textInputHandler = (textReceived) => {
     setTodo(textReceived);
   };
-
-  const handleClick = (e) => {
-    // Delete  an item from database
-    if (e === "delete") {
-      notificationHandler("Your todo has been deleted!");
-      remove(ref(db, id));
+  const handleClick = (name) => {
+    if (name === "delete") {
+      setUndo(true);
+      setTodo(text);
+      setTimeout(() => {
+        remove(ref(db, id));
+        notificationHandler("Your todo has been deleted!");
+      }, 1000);
     }
-    if (e === "done") {
+    if (name === "undo") {
+      // setTodo(text);
+      setUndo(false);
+      console.log(todo, id);
+      setTimeout(() => {
+        notificationHandler("Your todo has been restored!");
+        todo &&
+          set(ref(db, id), {
+            todo,
+            id,
+          });
+      }, 2000);
+    }
+
+    if (name === "done") {
       remove(ref(db, id));
       notificationHandler("CONGRATS! You have completed your task");
     }
 
     // Edit an item
-    if (e === "edit") {
+    if (name === "edit") {
       setEdit(true);
       setTodo(text);
     }
 
     // Update an item
-    if (e === "submit") {
+    if (name === "submit") {
       notificationHandler("Your todo has been updated!");
       update(ref(db, id), {
         todo,
@@ -44,18 +59,20 @@ export default function ListItem({ text, id, notificationHandler }) {
     }
     setTimeout(() => {
       notificationHandler("");
-    }, 2000);
+    }, 4000);
   };
 
   return (
     <li className={classes.listItem}>
-      {!edit && <p className={classes.listItemText}>{text}</p>}
+      {!edit && !undo && <p className={classes.listItemText}>{text}</p>}
+
       <div className={classes.listItemBtns}>
-        {!edit && (
+        {!edit && !undo && (
           <Button onClick={handleClick} btnType="btnDone" name="done">
             <i class="fa-solid fa-check"></i>
           </Button>
         )}
+
         {edit ? (
           <div className={classes.editText}>
             <TextArea textInput={textInputHandler} value={todo} />
@@ -63,19 +80,19 @@ export default function ListItem({ text, id, notificationHandler }) {
               Submit
             </Button>
           </div>
+        ) : undo ? (
+          <ListItemInfo onClick={handleClick} />
         ) : (
-          <Button onClick={handleClick} btnType="btnEdit" name="edit">
-            <i class="fa-solid fa-pen"></i>
-          </Button>
-        )}
-
-        {!edit && (
-          <Button onClick={handleClick} btnType="btnDelete" name="delete">
-            <i class="fa-solid fa-trash"></i>
-          </Button>
+          <>
+            <Button onClick={handleClick} btnType="btnEdit" name="edit">
+              <i class="fa-solid fa-pen"></i>
+            </Button>
+            <Button onClick={handleClick} btnType="btnDelete" name="delete">
+              <i class="fa-solid fa-trash"></i>
+            </Button>
+          </>
         )}
       </div>
-      {/* {notification && <Notification notification_msg={notification} />} */}
     </li>
   );
 }
--- src/components/ListItemInfo.js
+++ src/components/ListItemInfo.js
@@ -1,11 +1,16 @@
 import Button from "./Button";
 import classes from "../styles/ListItemInfo.module.css";
 
-export default function ListItemInfo() {
+export default function ListItemInfo(props) {
+  const handleClick = (e) => {
+    props.onClick(e);
+  };
   return (
-    <li className={classes.listItemInfo}>
+    <div className={classes.listItemInfo}>
       <p>Delete mistakenly? Press Undo fast!</p>
-      <Button btnType="btnUndo">Undo</Button>
-    </li>
+      <Button btnType="btnUndo" name="undo" onClick={handleClick}>
+        Undo
+      </Button>
+    </div>
   );
 }
